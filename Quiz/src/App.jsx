9eeacaf130f898661b10f0cf729e612495e1bb24
Harshit <hktiwari980@gmail.com>
2025-09-13 17:02:21 +0530

    developed UI and option added

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import QuestionCards from "./components/QuestionCards";
+import {questions} from "./data/questions"; 
+
 
 const App = () => {
+  const [currentQuestion, setCurrentQuestions] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [score, setScore] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+  const [showFeedback, setShowFeedback] = useState(false);
+
+  const handleAnswer = (option) =>{
+    if(showFeedback) return;
+
+    setSelectedAnswer(option);
+    setShowFeedback(true);
+    
+     if(option === questions[currentQuestion].answer){
+         setScore(score+1);
+     }
+  }
   return (
-    <div className='min-h-screen bg-gray-900 text-white flex items-center justify-center'>
-      <div className='text-center'>
-      <h1 className='text-4xl font-bold text-purple-600 mb-2'>Quiz App</h1>
+    <div className='min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center p-4'>
+      <div className='text-center mb-8'>
+      <h1 className='text-4xl font-bold text-purple-600 mb-2'>React Quiz App</h1>
       <p className='text-gray-400'>Test Your Knowledge</p>
     </div>
-    <questionCard/>
+    <QuestionCards data={questions[currentQuestion]}/>
     </div>
-  )
+  );
 }
 
 export default App
--- Quiz/src/components/questionCards.jsx
+++ Quiz/src/components/questionCards.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const questionCards = () => {
+const QuestionCards = ({data}) => {
+  const {question, options,answer} = data;
   return (
-    <div>
-      QuestionCard
+    <div className='bg-gray-800 p-6 rounded-2xl shadow-lg w-full
+      max-w-l border border-gray-700'>
+     <p>{question}</p>
+     <div className='grid gap-3'>
+      {options.map((option,index)=>(
+        <button 
+        className='text-left px-4 py-3 cursor-pointer bg-blue-800
+         rounded-lg text-white'
+        key={index}>{option}</button>
+      ))} 
+     </div>
     </div>
   )
 }
 
-export default questionCards;
+export default QuestionCards;
